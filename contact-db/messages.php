5deef8382fc521ce2a94010112adf9c214e87e3e
mariam <112480722+MariamHajir@users.noreply.github.com>
2022-09-14 12:22:41 +0300

    Allow Header Fixed.

--- contact-db/messages.php
+++ contact-db/messages.php
@@ -2,6 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
 include("connection.php");
 
 $full_name = isset($_POST['full_name']) ? $_POST['full_name'] : "";
